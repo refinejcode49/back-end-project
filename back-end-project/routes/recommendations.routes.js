5c09845583baac7c8014d03ab51bdfeb92dfbce9
Jennifer MERAT <131549759+refinejcode49@users.noreply.github.com>
2025-04-11 11:22:48 +0200

    minor fixes

--- back-end-project/routes/recommendations.routes.js
+++ back-end-project/routes/recommendations.routes.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 const express = require("express");
-const Recommendation = require("../models/Recommendation.model");
 const RecommendationModel = require("../models/Recommendation.model");
 
 // Get all recommendations (no mood filter)
 router.get("/all-recommendations", async (req, res) => {
   try {
-    const recommendations = await Recommendation.find();
+    const recommendations = await RecommendationModel.find();
     res.status(201).json(recommendations);
   } catch (error) {
     console.log(error);
@@ -22,7 +21,7 @@ router.get("/mood", async (req, res, next) => {
   try {
     //filter through mood in the model if no mood then return an empty object, no filtering and show all recommendations
     const filter = mood ? { mood: mood } : {};
-    const recos = await Recommendation.find(filter);
+    const recos = await RecommendationModel.find(filter);
     res.status(201).json(recos);
   } catch (err) {
     console.log(err);
@@ -35,7 +34,7 @@ router.get("/mood", async (req, res, next) => {
 router.post("/create-recommendation", async (req, res) => {
   const { category, title, creator, description, url, mood } = req.body;
   try {
-    const newReco = await Recommendation.create({
+    const newReco = await RecommendationModel.create({
       category,
       title,
       creator,
@@ -55,7 +54,7 @@ router.post("/create-recommendation", async (req, res) => {
 
 router.get("/recommendation/:id", async (req, res) => {
   try {
-    const reco = await Recommendation.findById(req.params.id);
+    const reco = await RecommendationModel.findById(req.params.id);
     if (!reco)
       return res.status(404).json({ message: "Recommendation not found" });
     res.json(reco);
@@ -66,7 +65,7 @@ router.get("/recommendation/:id", async (req, res) => {
 
 router.put("/recommendation/:id", async (req, res) => {
   try {
-    const updatedReco = await Recommendation.findByIdAndUpdate(
+    const updatedReco = await RecommendationModel.findByIdAndUpdate(
       req.params.id,
       req.body,
       { new: true }
@@ -83,7 +82,7 @@ router.put("/recommendation/:id", async (req, res) => {
 
 router.delete("/recommendation/:id", async (req, res) => {
   try {
-    await Recommendation.findByIdAndDelete(req.params.id);
+    await RecommendationModel.findByIdAndDelete(req.params.id);
     res.status(204).json({ message: "Recommendation deleted" });
   } catch (error) {
     console.log(error);
